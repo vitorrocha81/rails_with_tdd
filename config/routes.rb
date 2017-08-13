69d806000dcb0f21a7fa398ae1dbb94799f4bba1
Vitor Rocha <vitor.rocha81@bol.com.br>
2017-08-13 01:51:54 -0300

    validates user first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root to: "static#homepage"
   devise_for :users
- root to: "static#homepage"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+     @user = User.create(email: "teste@vitor.com",
+     password: "12345678", password_confirmation: "12345678",
+     first_name: "Vit", last_name: "Soprano")
+    end
+
+    it "create user ok" do
+      expect(@user).to be_valid
+    end
+
+    it "create user fail without first and last_name" do
+      @user.last_name = nil
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+  end  
 end
